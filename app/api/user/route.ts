955a941528c885e9d52fb55b07e032287735b3e8
sumitnair26 <sumit.nair26@gmail.com>
2024-03-08 15:56:53 +0530

    adding backend routes

---
+++ app/api/user/route.ts
@@ -0,0 +1,13 @@
+export function GET() {
+    return Response.json({
+        email:"sumit.nair26@gmail.com",
+        name:"Sumit Nair"
+    })
+}
+
+export function POST() {
+    return Response.json({
+        email:"sumit.nair26@gmail.com",
+        name:"Sumit Nair"
+    })
+}
