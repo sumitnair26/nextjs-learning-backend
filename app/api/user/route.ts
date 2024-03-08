09e27b4feeb9aa7d86b20cbcf9ea8cb3df43f5a4
sumitnair26 <sumit.nair26@gmail.com>
2024-03-09 01:05:15 +0530

    Better fetches

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,23 +2,31 @@ import { NextRequest } from "@/node_modules/next/server"
 import { PrismaClient } from "@/node_modules/.prisma/client/index";
 const client = new PrismaClient();
 
-export function GET() {
-    return Response.json({
-        email:"sumit.nair26@gmail.com",
-        name:"Sumit Nair"
-    })
-}
+// export async function GET() {
+//     const user = await client.user.findFirst();
+//     return Response.json({
+//         email:user?.username,
+//         name:user?.password
+//     })
+// }
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
-    console.log(body);
-    await client.user.create({
-        data: {
-            username: body.username,
-            password: body.password
-        }
-    })
-    return Response.json({
-        message: "You are logged in"
-    })
+   try {
+        await client.user.create({
+            data: {
+                username: body.username,
+                password: body.password
+            }
+        })
+        return Response.json({
+            message: "User Added"
+        })
+   } catch (error) {
+        return Response.json({
+            message: "Error Something went wrong"
+        },{
+            status:411
+        })
+   }
 } 
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
-import axios from "axios";
+//import axios from "axios";
+import { PrismaClient } from "@/node_modules/.prisma/client/index";
+const client = new PrismaClient();
 
 async function getUserDetails() {
-  const response = await axios.get("http://localhost:3003/api/user")
-	return response.data;
+  console.log('function has been called');
+  // const response = await axios.get("http://localhost:3003/api/user")
+	// return response.data;
+
+  try {
+    const user = await client.user.findFirst();
+    return {
+      email:user?.username,
+      name:user?.password
+    }
+
+  } catch (error) {
+      console.log(error);
+  }
+
 }
 
 export default async function Home() {
